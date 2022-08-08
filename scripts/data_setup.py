2edd1e2dc8931f94a11bf7da4a86559129405fa3
smoran3 <smoran@dvrpc.org>
2022-08-08 10:26:19 -0400

    finished data setup troubleshooting

--- scripts/data_setup.py
+++ scripts/data_setup.py
@@ -1,65 +1,76 @@
 """
-get_data.py
+data_setup.py
 ------------------
 This script pulls the latest crash datatable from the GIS database.
-It then clips that data to the a 100 ft buffer around the linear study area.
-If the study area is a polygon instead and no buffer is needed, update lines...
-Results are written to shapefile and postgres
+It then clips that data to a 100 ft buffer around a linear study area.
+The result of this function is a geodataframe containing crashes within the study area.
 """
 
-import csv
-from datetime import date
-import pandas as pd
 import geopandas as gpd
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import create_engine
+from shapely import geometry, ops
 import env_vars as ev
 from env_vars import GIS_ENGINE, ENGINE
 
-sa_shape = "Hunting_Park_Study_Area_"
-sa_name = "Hunting_Park"
 
 def main():
 
-    #read study area shapefile and create buffer
-    study_area = gpd.GeoDataFrame.read_file(fr"{ev.DATA_ROOT}/{sa_shape}.shp")
-    sa_buffer = study_area.buffer(100)
+    sa_shape = "Hunting_Park_Study_Area_"
+    sa_name = "Hunting_Park"
 
+    #create database and enable postgis
+    if not database_exists(ENGINE.url):
+        create_database(ENGINE.url)
+    ENGINE.execute("CREATE EXTENSION IF NOT EXISTS postgis;")
+
+    #read study area shapefile and write to postgres
+    study_area = gpd.read_file(fr"{ev.DATA_ROOT}/{sa_shape}.shp")
+    study_area.to_postgis('study_area', con=ENGINE, if_exists="replace")
+ 
+    #create 100ft buffer around study area and save as new table
+    ENGINE.execute("""
+        CREATE TABLE sa_buffer AS(
+        select st_transform(st_buffer(st_linemerge(st_union(geometry)),100), 4326) as buff
+        from study_area sa);""")
 
     #read crash data from gis database
-
     crash_data = gpd.GeoDataFrame.from_postgis(
-        #this query should be broad and include everything you would want for generating charts
+        #this query should be broad and include everything needed to generate charts
         #joins to other tables should happen before or within this querey
-        #this simple query is here is an example
         fr"""select 
                 crash_year, 
                 county, 
                 fatal_count , 
                 maj_inj_count, 
-                shape 
-            from crash_pennsylvania cp 
+                shape
+            from transportation.crash_pennsylvania cp 
             where district = '06'
             and crash_year = '2019'
-            and county = '67';""", 
-        con = GIS_ENGINE
+            and county = '67'
+            and shape is not null;""", 
+        con = GIS_ENGINE,
         geom_col = "shape",
     )
+    #write to postgis
+    crash_data.to_postgis('crash_data', con=ENGINE, if_exists="replace")
 
-    #clip crash data to buffer
-    sa_crashes = gpd.clip(crash_data, sa_buffer)
+    sa_crashes = gpd.GeoDataFrame.from_postgis(
+        """SELECT cd.*
+        FROM crash_data cd JOIN sa_buffer sa ON ST_Intersects(cd.shape, sa.buff) """,
+        con = ENGINE,
+        geom_col= "shape"
+    )
 
     #export clipped crash data to shapefile
     sa_crashes.to_file(fr"{ev.DATA_ROOT}/{sa_name}_crashes.shp")
     print("To shapefile: Complete")
 
-    #create database
-    if not database_exists(ENGINE.url):
-        create_database(ENGINE.url)
-    ENGINE.execute("CREATE EXTENSION IF NOT EXISTS postgis;")
-
     #write dataframe to postgres database
     sa_crashes.to_postgis(fr"{sa_name}_crashes", con=ENGINE, if_exists="replace")
+    print("To postgis: Complete")
 
+    return sa_crashes
 
 if __name__ == "__main__":
     main()
