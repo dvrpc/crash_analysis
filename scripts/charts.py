3e10cd8c8bbfac75b669c78d33a109d8e1d10595
Mark Morley <mmorley0395@gmail.com>
2022-08-25 11:47:16 -0400

    added another chart

--- scripts/charts.py
+++ scripts/charts.py
@@ -10,23 +10,39 @@ from data_setup import clip_crashes
 import env_vars as ev
 
 df = clip_crashes()
+year=df["crash_year"].median() #placeholder until year is a variable in the main function
+total_crashes = len(df.index)
+injury_count= df["maj_inj_count"].sum()
+fatality_count=df["fatal_count"].sum()
+ped_count=df["ped_count"].sum()
+bicycle_count=df["bicycle_count"].sum()
+total_minus_fatal_and_inj = (total_crashes-injury_count-fatality_count)
+total_minus_bike_ped_count = (total_crashes-ped_count-bicycle_count)
 
-def pie_chart(df):
+def injury_pie_chart(df):
     """
     Creates a pie chart for injuries and fatalities versus total crashes in the study area
     """
-    year=df["crash_year"].median() #placeholder until year is a variable in the main function
-    total_crashes = len(df.index)
-    injury_count= df["maj_inj_count"].sum()
-    fatality_count=df["fatal_count"].sum()
-    total_minus_fatal_and_inj = (total_crashes-injury_count-fatality_count)
     data = [injury_count,fatality_count,total_minus_fatal_and_inj]
     labels = ["Injuries", "Fatalities", "Other Crashes"]
     plt.title(f'{total_crashes} Crashes in Study Area in {round(year)}')
     plt.pie(x=data, labels=labels, autopct= lambda x: '{:.0f}'.format(x*sum(data)/100))
     plt.savefig(f'{ev.DATA_ROOT}/injury_fatality_piechart.png')
+    plt.cla()
 
+def mode_pie_chart(df):
+    """
+    Creates a pie chart for breakdown of crashes by mode
+    """
+    data= [ped_count,bicycle_count,total_minus_bike_ped_count]
+    mode_labels = ["Pedestrian", "Cyclist", "Vehicular Crashes"]
+    plt.title(f'{total_crashes} Crashes in Study Area in {round(year)}')
+    plt.pie(x=data, labels=mode_labels, autopct= lambda x: '{:.0f}'.format(x*sum(data)/100))
+    plt.savefig(f'{ev.DATA_ROOT}/mode_piechart.png')
+    plt.cla()
 
 
 if __name__ == "__main__":
-    pie_chart(df)
+    print("Generating charts.. See project directory for images...")
+    injury_pie_chart(df)
+    mode_pie_chart(df)
