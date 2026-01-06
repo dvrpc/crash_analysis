d299aaa7373b21eb03f5ef1961cd9bc255363152
smoran3 <smoran@dvrpc.org>
2026-01-06 13:52:09 -0500

    updated queries with new data types and field names

--- scripts/crash_queries.py
+++ scripts/crash_queries.py
@@ -12,14 +12,14 @@ q_mode_severity = fr"""
 		SELECT 
 			COUNT(crn) AS crash_count,
 			CASE
-				WHEN ped_count > 0 THEN 'Pedestrian-involved'
-				WHEN bicycle_count > 0 THEN 'Bicyclist-involved'
+				WHEN ped_count::integer > 0 THEN 'Pedestrian-involved'
+				WHEN bicycle_count::integer > 0 THEN 'Bicyclist-involved'
 				ELSE 'Vehicle Occupants Only'
 			END AS Mode,
 			CASE
-				WHEN max_severity_level = 0 THEN 'pdo'
-				WHEN max_severity_level IN(1,2) THEN 'ksi'
-				WHEN max_severity_level IN (3,4,8,9) THEN 'non-ksi-inj'
+				WHEN max_severity_level::integer = 0 THEN 'pdo'
+				WHEN max_severity_level::integer IN(1,2) THEN 'ksi'
+				WHEN max_severity_level::integer IN (3,4,8,9) THEN 'non-ksi-inj'
 			END AS max_severity
 		FROM crash_data
 		GROUP BY mode, max_severity
@@ -38,14 +38,14 @@ q_mode_severity = fr"""
 		SELECT 
 			COUNT(crn) AS crash_count,
 			CASE
-				WHEN ped_count > 0 THEN 'Pedestrian-involved'
-				WHEN bicycle_count > 0 THEN 'Bicyclist-involved'
+				WHEN ped_count::integer > 0 THEN 'Pedestrian-involved'
+				WHEN bicycle_count::integer > 0 THEN 'Bicyclist-involved'
 				ELSE 'Vehicle Occupants Only'
 			END AS mode,
 			CASE
-				WHEN max_severity_level = 0 THEN 'pdo'
-				WHEN max_severity_level IN(1,2) THEN 'ksi'
-				WHEN max_severity_level IN (3,4,8,9) THEN 'non-ksi-inj'
+				WHEN max_severity_level::integer = 0 THEN 'pdo'
+				WHEN max_severity_level::integer IN(1,2) THEN 'ksi'
+				WHEN max_severity_level::integer IN (3,4,8,9) THEN 'non-ksi-inj'
 			END AS max_severity
 		FROM crash_data
 		GROUP BY mode, max_severity
@@ -63,9 +63,9 @@ FROM (
         COUNT(crn) AS crash_count,
         crash_year,
         CASE
-            WHEN max_severity_level = 0 THEN 'pdo'
-            WHEN max_severity_level IN(1,2) THEN 'ksi'
-            WHEN max_severity_level IN (3,4,8,9) THEN 'non-ksi-inj'
+            WHEN max_severity_level::integer = 0 THEN 'pdo'
+            WHEN max_severity_level::integer IN(1,2) THEN 'ksi'
+            WHEN max_severity_level::integer IN (3,4,8,9) THEN 'non-ksi-inj'
         END AS max_severity
     FROM crash_data
     GROUP BY crash_year, max_severity_level
@@ -75,44 +75,47 @@ ORDER BY crash_year; """
 	
 #Crashes by collision type and severity
 q_crashes_by_type = fr"""
-SELECT 
+select * from (
+SELECT
     collision_type,
     SUM(CASE WHEN max_severity = 'pdo' THEN crash_count ELSE 0 END) AS "PDO",
     SUM(CASE WHEN max_severity = 'ksi' THEN crash_count ELSE 0 END) AS "KSI",
     SUM(CASE WHEN max_severity = 'non-ksi-inj' THEN crash_count ELSE 0 END) AS "Non-KSI Injury",
     SUM(crash_count) AS "Total"
 FROM (
-    SELECT 
-        COUNT(crn) AS crash_count,
-        collision_type,
+    SELECT
+        COUNT(cd.crn) AS crash_count,
+        cd.collision_type AS collision_type,
         CASE
-            WHEN max_severity_level = 0 THEN 'pdo'
-            WHEN max_severity_level IN(1,2) THEN 'ksi'
-            WHEN max_severity_level IN (3,4,8,9) THEN 'non-ksi-inj'
+            WHEN max_severity_level::integer = 0 THEN 'pdo'
+            WHEN max_severity_level::integer IN(1,2) THEN 'ksi'
+            WHEN max_severity_level::integer IN (3,4,8,9) THEN 'non-ksi-inj'
         END AS max_severity
-    FROM crash_data
+    FROM crash_data cd
     GROUP BY collision_type, max_severity
 ) AS subquery
 GROUP BY collision_type
-
 UNION ALL
-
-SELECT 
+SELECT
     'Total' AS collision_type,
     SUM(CASE WHEN max_severity = 'pdo' THEN crash_count ELSE 0 END) AS "PDO",
     SUM(CASE WHEN max_severity = 'ksi' THEN crash_count ELSE 0 END) AS "KSI",
     SUM(CASE WHEN max_severity = 'non-ksi-inj' THEN crash_count ELSE 0 END) AS "Non-KSI Injury",
     SUM(crash_count) AS "Total"
 FROM (
-    SELECT 
-        COUNT(crn) AS crash_count,
-        collision_type,
+    SELECT
+        COUNT(cd.crn) AS crash_count,
+        cd.collision_type,
         CASE
-            WHEN max_severity_level = 0 THEN 'pdo'
-            WHEN max_severity_level IN(1,2) THEN 'ksi'
-            WHEN max_severity_level IN (3,4,8,9) THEN 'non-ksi-inj'
+            WHEN max_severity_level::integer = 0 THEN 'pdo'
+            WHEN max_severity_level::integer IN(1,2) THEN 'ksi'
+            WHEN max_severity_level::integer IN (3,4,8,9) THEN 'non-ksi-inj'
         END AS max_severity
-    FROM crash_data
-    GROUP BY collision_type, max_severity
-) AS subquery; """
+    FROM crash_data cd
+    GROUP BY cd.collision_type, max_severity
+) AS subquery
+) as final_result
+ORDER BY 
+    CASE WHEN collision_type = 'Total' THEN 1 ELSE 0 END,
+    collision_type;"""
 	
